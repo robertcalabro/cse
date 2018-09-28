e76b1ae6c4b2a279d27b423147d316fdfd10fe35
robertcalabro <43617864+robertcalabro@users.noreply.github.com>
2018-09-28 11:41:41 +0200

    Update CSE_01_Install-FileServices.ps1

--- CSE_01_Install-FileServices.ps1
+++ CSE_01_Install-FileServices.ps1
@@ -16,4 +16,12 @@ Start-Transcript "$tmpDir\ScriptExtension.log" -Append
 $features = @("FileAndStorage-Services","File-Services", "FS-FileServer", "FS-Data-Deduplication", "Storage-Services", "AD-Domain-Services")
 Install-WindowsFeature -Name $features -IncludeManagementTools -IncludeAllSubFeature -Verbose
 
+#Create a storage Pool with a VDisk and Volume
+$StaPool01 = New-StoragePool -FriendlyName "DataStaPool01" -StorageSubSystemUniqueId (Get-StorageSubSystem -FriendlyName "*storage*").uniqueID -PhysicalDisks (Get-PhysicalDisk -CanPool $true)
+$VDisk01 = New-VirtualDisk -FriendlyName "VData01" -StoragePoolFriendlyName $StaPool01.FriendlyName -UseMaximumSize -ProvisioningType Fixed -ResiliencySettingName simple
+$Disknumber = Get-VirtualDisk -FriendlyName $VDisk01.FriendlyName | Get-Disk | Select-Object Number
+Initialize-Disk -Number $Disknumber.Number
+$NewPartition = New-Partition -DiskNumber $Disknumber.Number -UseMaximumSize -AssignDriveLetter
+Format-Volume -DriveLetter $NewPartition.DriveLetter -FileSystem NTFS -NewFileSystemLabel "DataStore01"
+
 Stop-Transcript
