75266f47d849324dca90c05c1b21a1dd0c3c3165
robertcalabro <43617864+robertcalabro@users.noreply.github.com>
2018-10-02 15:52:12 +0200

    Update CSE_01_Install-FileServices.ps1

--- CSE_01_Install-FileServices.ps1
+++ CSE_01_Install-FileServices.ps1
@@ -6,13 +6,107 @@ Add Domain Forest
 ############################
 #   Function
 ############################
-function generate-password {
+    .FUNCTIONALITY
+       Generates random passwords
+  
+    #>
+    [CmdletBinding(DefaultParameterSetName='FixedLength',ConfirmImpact='None')]
+    [OutputType([String])]
+    Param
+    (
+        # Specifies minimum password length
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='RandomLength')]
+        [ValidateScript({$_ -gt 0})]
+        [Alias('Min')] 
+        [int]$MinPasswordLength = 8,
+        
+        # Specifies maximum password length
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='RandomLength')]
+        [ValidateScript({
+                if($_ -ge $MinPasswordLength){$true}
+                else{Throw 'Max value cannot be lesser than min value.'}})]
+        [Alias('Max')]
+        [int]$MaxPasswordLength = 12,
 
-   $passwordlength = 8   # length of dynamic generates password
-   $passwortchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" # characters for password generation
+        # Specifies a fixed password length
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='FixedLength')]
+        [ValidateRange(1,2147483647)]
+        [int]$PasswordLength = 8,
+        
+        # Specifies an array of strings containing charactergroups from which the password will be generated.
+        # At least one char from each group (string) will be used.
+        [String[]]$InputStrings = @('abcdefghijkmnpqrstuvwxyz', 'ABCEFGHJKLMNPQRSTUVWXYZ', '23456789', '!"#%&'),
 
-   -join ($passwortchars.tochararray() | get-random -count $passwordlength | foreach {$_}) 
+        # Specifies a string containing a character group from which the first character in the password will be generated.
+        # Useful for systems which requires first char in password to be alphabetic.
+        [String] $FirstChar,
+        
+        # Specifies number of passwords to generate.
+        [ValidateRange(1,2147483647)]
+        [int]$Count = 1
+    )
+    Begin {
+        Function Get-Seed{
+            # Generate a seed for randomization
+            $RandomBytes = New-Object -TypeName 'System.Byte[]' 4
+            $Random = New-Object -TypeName 'System.Security.Cryptography.RNGCryptoServiceProvider'
+            $Random.GetBytes($RandomBytes)
+            [BitConverter]::ToUInt32($RandomBytes, 0)
+        }
+    }
+    Process {
+        For($iteration = 1;$iteration -le $Count; $iteration++){
+            $Password = @{}
+            # Create char arrays containing groups of possible chars
+            [char[][]]$CharGroups = $InputStrings
+
+            # Create char array containing all chars
+            $AllChars = $CharGroups | ForEach-Object {[Char[]]$_}
+
+            # Set password length
+            if($PSCmdlet.ParameterSetName -eq 'RandomLength')
+            {
+                if($MinPasswordLength -eq $MaxPasswordLength) {
+                    # If password length is set, use set length
+                    $PasswordLength = $MinPasswordLength
+                }
+                else {
+                    # Otherwise randomize password length
+                    $PasswordLength = ((Get-Seed) % ($MaxPasswordLength + 1 - $MinPasswordLength)) + $MinPasswordLength
+                }
+            }
+
+            # If FirstChar is defined, randomize first char in password from that string.
+            if($PSBoundParameters.ContainsKey('FirstChar')){
+                $Password.Add(0,$FirstChar[((Get-Seed) % $FirstChar.Length)])
+            }
+            # Randomize one char from each group
+            Foreach($Group in $CharGroups) {
+                if($Password.Count -lt $PasswordLength) {
+                    $Index = Get-Seed
+                    While ($Password.ContainsKey($Index)){
+                        $Index = Get-Seed                        
+                    }
+                    $Password.Add($Index,$Group[((Get-Seed) % $Group.Count)])
+                }
+            }
+
+            # Fill out with chars from $AllChars
+            for($i=$Password.Count;$i -lt $PasswordLength;$i++) {
+                $Index = Get-Seed
+                While ($Password.ContainsKey($Index)){
+                    $Index = Get-Seed                        
+                }
+                $Password.Add($Index,$AllChars[((Get-Seed) % $AllChars.Count)])
+            }
+            Write-Output -InputObject $(-join ($Password.GetEnumerator() | Sort-Object -Property Name | Select-Object -ExpandProperty Value))
+        }
+    }
 }
+
 
 ###################################################################
 
@@ -43,7 +137,7 @@ $ADPathDir = "f:\ADDB"
 if (!(Test-Path $ADPathDir)) {mkdir $ADPathDir -force}
 
 #Password Gernerator
-$pwd = generate-password
+$pwd = New-SWRandomPassword -MinPasswordLength 8 -MaxPasswordLength 12
 $SecurePassword = ConvertTo-SecureString $pwd -AsPlainText -Force
 
 #Install AD Forest
