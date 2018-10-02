0c3c17ff4290aec362b79c63b64f676a326b9012
robertcalabro <43617864+robertcalabro@users.noreply.github.com>
2018-10-02 13:46:12 +0200

    Update CSE_01_Install-FileServices.ps1

--- CSE_01_Install-FileServices.ps1
+++ CSE_01_Install-FileServices.ps1
@@ -1,6 +1,20 @@
 <#
-this custom script extension installs file services
+this custom script extension installs File, AD Feature.
+Add Storage Pool, VDisk and Volume
+Add Domain Forest
 #>
+############################
+#   Function
+############################
+function generate-password {
+
+   $passwordlength = 8   # length of dynamic generates password
+   $passwortchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" # characters for password generation
+
+   -join ($passwortchars.tochararray() | get-random -count $passwordlength | foreach {$_}) 
+}
+
+###################################################################
 
 
 #this will be our temp folder - need it for download / logging
@@ -10,11 +24,11 @@ $tmpDir = "c:\temp\"
 #create folder if it doesn't exist
 if (!(Test-Path $tmpDir)) { mkdir $tmpDir -force}
 
-Start-Transcript "$tmpDir\ScriptExtension.log" -Append
+Start-Transcript "$tmpDir\ScriptExtension_DC_FS.log" -Append
 
 #install Fileservice and Domain features 
 $features = @("FileAndStorage-Services","File-Services", "FS-FileServer", "FS-Data-Deduplication", "Storage-Services", "AD-Domain-Services")
-Install-WindowsFeature -Name $features -IncludeManagementTools -IncludeAllSubFeature -Verbose
+Install-WindowsFeature -Name $features -Verbose -IncludeManagementTools -IncludeAllSubFeature
 
 #Create a storage Pool with a VDisk and Volume
 $StaPool01 = New-StoragePool -FriendlyName "DataStaPool01" -StorageSubSystemUniqueId (Get-StorageSubSystem -FriendlyName "*storage*").uniqueID -PhysicalDisks (Get-PhysicalDisk -CanPool $true)
@@ -23,5 +37,16 @@ $Disknumber = Get-VirtualDisk -FriendlyName $VDisk01.FriendlyName | Get-Disk | S
 Initialize-Disk -Number $Disknumber.Number
 $NewPartition = New-Partition -DiskNumber $Disknumber.Number -UseMaximumSize -AssignDriveLetter
 Format-Volume -DriveLetter $NewPartition.DriveLetter -FileSystem NTFS -NewFileSystemLabel "DataStore01"
+
+#Check Path
+$ADPathDir = "f:\ADDB"
+if (!(Test-Path $ADPathDir)) {mkdir $ADPathDir -force}
+
+#Password Gernerator
+$pwd = generate-password
+$SecurePassword = ConvertTo-SecureString $pwd -AsPlainText -Force
+
+#Install AD Forest
+Install-ADDSForest -DomainName mc-demo.de -DomainNetBiosName mc-demo -DomainMode WinThreshold -ForestMode WinThreshold -SkipPreChecks -DatabasePath $ADPathDir -InstallDns:$true -SafeModeAdministratorPassword $SecurePassword -Force
 
 Stop-Transcript
