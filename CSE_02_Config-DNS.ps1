18c0d47773b3707ad8d72e875235079d2b7c98d1
robertcalabro <43617864+robertcalabro@users.noreply.github.com>
2018-10-02 13:44:24 +0200

    Add files via upload

---
+++ CSE_02_Config-DNS.ps1
@@ -0,0 +1,26 @@
+﻿<#
+this custom script extension configer DNS.
+
+#>
+
+#this will be our temp folder - need it for download / logging
+
+$tmpDir = "c:\temp\" 
+
+#create folder if it doesn't exist
+if (!(Test-Path $tmpDir)) { mkdir $tmpDir -force}
+
+Start-Transcript "$tmpDir\ScriptExtension_DNS.log" -Append
+
+#DNS Configure
+#Reverse-Lookup-Zone erstellen
+Add-DnsServerPrimaryZone -NetworkID 10.0.0.0/24 -ReplicationScope Domain -DynamicUpdate Secure -PassThru
+
+#DNS Regesrieren
+ipconfig /registerdns
+
+#DNS Eintrag auf die Server IP änderen
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet 2" -ResetServerAddresses
+
+
+Stop-Transcript
