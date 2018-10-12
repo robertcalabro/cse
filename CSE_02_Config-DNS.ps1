910cf5f5279e23af3c53e2ebe6cd9a3f9495186e
robertcalabro <43617864+robertcalabro@users.noreply.github.com>
2018-10-12 14:35:54 +0200

    Update CSE_02_Config-DNS.ps1

--- CSE_02_Config-DNS.ps1
+++ CSE_02_Config-DNS.ps1
@@ -14,8 +14,8 @@ Start-Transcript "$tmpDir\ScriptExtension_DNS.log" -Append
 
 #DNS Configure
 #Reverse-Lookup-Zone erstellen
-Add-DnsServerPrimaryZone -NetworkID 10.0.1.0/24 -ReplicationScope Domain -DynamicUpdate Secure -PassThru
-Add-DnsServerPrimaryZone -NetworkID 10.0.2.0/24 -ReplicationScope Domain -DynamicUpdate Secure -PassThru
+Add-DnsServerPrimaryZone -NetworkID 10.1.0.0/24 -ReplicationScope Domain -DynamicUpdate Secure -PassThru
+Add-DnsServerPrimaryZone -NetworkID 10.2.0.0/24 -ReplicationScope Domain -DynamicUpdate Secure -PassThru
 
 #DNS Regesrieren
 ipconfig /registerdns
@@ -23,5 +23,10 @@ ipconfig /registerdns
 #DNS Eintrag auf die Server IP änderen
 Set-DnsClientServerAddress -InterfaceAlias "Ethernet 2" -ResetServerAddresses
 
+#Erstellen eines AD Admin
+$password = "Welcome2018!" | ConvertTo-SecureString -AsPlainText -Force
+$DNSroot = (Get-ADDomain).dnsroot
+New-ADUser -Name Deployment -UserPrincipalName deployment@$dnsroot -AccountPassword $Password -PasswordNeverExpires $True -Enabled $True
+Add-ADGroupMember -Identity "Domain Admins" -Members deployment
 
 Stop-Transcript
